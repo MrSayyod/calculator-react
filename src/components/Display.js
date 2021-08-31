147270d0c718d9668cbc21d37ef68b2ee9b139c1
MrSayyod <ssayyod@gmail.com>
2021-08-31 13:24:02 -0600

    Add rendering components into Display.js

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{props.result}</h1>
+      </div>
+    )
+  }
+}
+
+Display.propTypes = {
+  result: PropTypes.string.isRequired
+}
