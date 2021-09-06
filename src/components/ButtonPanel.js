e0ef88dd5482b91abadd8b25d9adbea086cbf7b2
MrSayyod <ssayyod@gmail.com>
2021-09-06 15:18:58 -0600

    Add class for button 0

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -27,7 +27,7 @@ const ButtonPanel = () => (
       <button type="button">+</button>
     </div>
     <div className="group_5">
-      <button type="button">0</button>
+      <button type="button" className="button_zero">0</button>
       <button type="button">.</button>
       <button type="button">=</button>
     </div>
