8399f690b8a16dad44e075dc98c80d815a79dd8b
MrSayyod <ssayyod@gmail.com>
2021-09-08 16:33:30 -0600

    Add adding function

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,15 @@
 import Big from 'big.js'
 
 const operate = (numberOne, numberTwo, operation) => {
+  let result = 0
+  const one = Big(numberOne)
+  const two = Big(numberTwo)
+  switch (operation) {
+    case '+':
+      result = one.plus(two)
+      break;
   
+    default:
+      break;
+  }
 }
