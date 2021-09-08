68889c02b71af9a14ae260cc56288e6917b24494
MrSayyod <ssayyod@gmail.com>
2021-09-08 17:15:28 -0600

    Add logic for +/- button

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,7 +1,10 @@
+import operate from "./operate"
+
 const calculate = (data, buttonName) => {
   const { total, next, operation } = data
   const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
   const operators = ['+', '-', '*', '/', '%', '=']
+  
   if (numbers.includes(buttonName)) {
     if (total === '0' && !total) {
       total = buttonName
@@ -16,4 +19,14 @@ const calculate = (data, buttonName) => {
       next += buttonName
     }
   }
+
+  if (buttonName === '+/-') {
+    if (total && !next) {
+      total = operate(total, -1, '*')
+    } else if (total && next) {
+      next = operate(next, -1, '*')
+    }
+  }
+
+  
 }
