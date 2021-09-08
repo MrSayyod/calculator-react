1e7d3138b31e32ecc6663d9621d36ffaf22738fb
MrSayyod <ssayyod@gmail.com>
2021-09-08 17:04:07 -0600

    Add logic for starting position

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,4 +1,10 @@
 const calculate = (data, buttonName) => {
-  const {total, next, operation} = data
-  
+  const { total, next, operation } = data
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  const operators = ['+', '-', '*', '/', '%', '=']
+  if (numbers.includes(buttonName)) {
+    if (total === '0' && !total) {
+      total = buttonName
+    }
+  }
 }
