0de3514ce25d384720aab2183e1bec8d73c9f48f
MrSayyod <ssayyod@gmail.com>
2021-09-09 15:48:47 -0600

    Import component and calculate method

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
 const App = () => (
   <>
