537d382dce7f2e7e082417e890898680432d8169
MrSayyod <ssayyod@gmail.com>
2021-09-09 17:01:14 -0600

    Set state

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,22 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+// const App = () => (
+//   <>
+//     <Display />
+//     <ButtonPanel />
+//   </>
+// );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+}
 
 export default App;
