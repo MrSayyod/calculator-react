b5459f45a67aed6f370d40407f8cc27c422643ae
MrSayyod <ssayyod@gmail.com>
2021-09-21 13:11:59 -0600

    Implement return method

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 // import React, { Component } from 'react';
-import React, { useState } from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
+import React, { useState } from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
 
 // class App extends Component {
 //   constructor(props) {
@@ -33,19 +33,23 @@ import calculate from '../logic/calculate';
 // }
 
 const App = () => {
-  const [total, setTotal] = useState('null')
-  const [next, setNext] = useState('null')
-  const [operation, setOperation] = useState('null')
+  const [total, setTotal] = useState("null");
+  const [next, setNext] = useState("null");
+  const [operation, setOperation] = useState("null");
 
   const handleClick = (buttonName) => {
-    const newResult = calculate({ total, next, operation }, buttonName)
-    setTotal(newResult.total)
-    setNext(newResult.next)
-    setOperation(newResult.operation)
-  }
+    const newResult = calculate({ total, next, operation }, buttonName);
+    setTotal(newResult.total);
+    setNext(newResult.next);
+    setOperation(newResult.operation);
+  };
 
-  
-}
-
+  return (
+    <div className="displayResult">
+      <Display total={next || total || "0"} />
+      <ButtonPanel handleClick={this.handleClick} />
+    </div>
+  );
+};
 
 export default App;
