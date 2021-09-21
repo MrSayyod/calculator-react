ccff3becf5bae986547120d1c2a23a4f09d18b04
MrSayyod <ssayyod@gmail.com>
2021-09-21 13:03:09 -0600

    Import useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//     };
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+//     this.handleClick = this.handleClick.bind(this);
+//   }
 
-  handleClick =(buttonName) => {
-    const newResult = calculate(this.state, buttonName);
-    this.setState(newResult);
-  }
+//   handleClick =(buttonName) => {
+//     const newResult = calculate(this.state, buttonName);
+//     this.setState(newResult);
+//   }
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="displayResult">
-        <Display total={next || total || '0'} />
-        <ButtonPanel handleClick={this.handleClick} />
-      </div>
-    );
-  }
-}
+//   render() {
+//     const { total, next } = this.state;
+//     return (
+//       <div className="displayResult">
+//         <Display total={next || total || '0'} />
+//         <ButtonPanel handleClick={this.handleClick} />
+//       </div>
+//     );
+//   }
+// }
+
 
 export default App;
