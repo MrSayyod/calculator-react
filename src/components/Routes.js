d913d77f05c97ddc381fd6f5b01e90f4ac7954d2
MrSayyod <ssayyod@gmail.com>
2021-09-23 13:24:44 -0600

    Create Routes

---
+++ src/components/Routes.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import App from "./App";
+import Home from "../layouts/Home";
+import Quotes from "../layouts/Quote";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={App} />
+        <Route exact path="/quotes" component={Quotes} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes
