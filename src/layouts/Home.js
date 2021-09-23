629cf6ae0f836ed870e40885c666ea5a21f76090
MrSayyod <ssayyod@gmail.com>
2021-09-23 13:09:06 -0600

    Add elements into Home.js

---
+++ src/layouts/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => (
+  <>
+    <Navbar />
+    <main>
+      <section className="area_for_welcome">
+        <h3>Welcome to our page!</h3>
+      </section>
+      <section className="area_for_text">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem, error? Totam fugit aliquid vero voluptates
+          consequatur ex consequuntur quisquam. Esse aspernatur quia aut dolor
+          adipisci illum dicta minima sed blanditiis. Quasi at temporibus
+          eveniet voluptate modi quaerat assumenda ad eos voluptatum aliquid.
+          Cumque velit sint, quasi minus vel, perferendis id quo, eum sit non
+          pariatur. Sed quo facere nam velit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+          mollitia voluptas fuga provident vel cupiditate odio impedit
+          reprehenderit. Non officiis tempora unde quos consectetur laboriosam
+          magni aliquam laborum numquam sed! Qui officia impedit dicta quia
+          porro quae rerum fugiat iste, voluptatem minima! Quod ut accusamus
+          in explicabo repellendus, recusandae veniam illum dolore iure optio
+          officiis corrupti blanditiis neque fugiat reiciendis?
+        </p>
+      </section>
+    </main>
+  </>
+);
+
+export default Home;
