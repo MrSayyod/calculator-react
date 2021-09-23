9f6eec9d61d61146c3c06910e6412ef8221d64ee
MrSayyod <ssayyod@gmail.com>
2021-09-23 12:55:04 -0600

    Add navbar

---
+++ src/layouts/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <div>Math Magicians</div>
+        <div>
+          <ul>
+            <li>
+            
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
