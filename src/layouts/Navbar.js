eadfcb2974411c7d244559a959a79a623a9f1ba2
MrSayyod <ssayyod@gmail.com>
2021-09-23 13:00:33 -0600

    Add router and links to navbar

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
+const Navbar = () => (
+  <BrowserRouter>
     <header>
       <nav>
-        <div>Math Magicians</div>
-        <div>
+        <div className="leftNav">Math Magicians</div>
+        <div className="rightNav">
           <ul>
             <li>
-            
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/quotes">Quotes</Link>
             </li>
           </ul>
         </div>
       </nav>
     </header>
-  );
-};
+  </BrowserRouter>
+);
+
+export default Navbar;
