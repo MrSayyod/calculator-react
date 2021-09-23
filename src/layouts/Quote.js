b5809f82b8650a2d30e8bab9a109fd762359fba2
MrSayyod <ssayyod@gmail.com>
2021-09-23 13:19:34 -0600

    Add elements and finish Quotes page

---
+++ src/layouts/Quote.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Quotes = () => (
+  <>
+    <Navbar />
+    <main>
+      <section className="area_for_quotes">
+        <h4>Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. - <span>William Paul Thurston</span></h4>
+      </section>
+    </main>
+  </>
+);
+
+export default Quotes;
