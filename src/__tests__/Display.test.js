906ab1b64bd51ba65b967f81362cfb8bde7a2eec
MrSayyod <ssayyod@gmail.com>
2021-09-25 15:24:21 -0600

    Add test for Display component

---
+++ src/__tests__/Display.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "../components/Display";
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("displays the result", () => {
+  const { renderer } = render(<Display total="1" />);
+  expect(screen.getByTestId("display-test")).toHaveTextContent("1");
+  expect(renderer).toMatchSnapshot();
+});
--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Display = (props) => {
   const { total, next, operation } = props;
   return (
-    <div className="display">
+    <div className="display" data-testid="display-test">
       { total }
       { ' ' }
       { operation }
